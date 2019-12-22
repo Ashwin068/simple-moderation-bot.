2ee190f67600d60fd37b65a0d1310301620f5205
Ashwin <56057445+Ashwin1172@users.noreply.github.com>
2019-12-22 12:27:13 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-G
+const codDevClient = require('./handle/codDevClient');
+const client = new codDevClient({
+  fetchAllMembers: true,
+  disabledEvents: ["TYPING_START", "USER_NOTE_UPDATE"],
+  disableEveryone: true
+});
+
+require('./handle/events')(client);
+require('./handle/module')(client);
+
+client.login("NjU3OTg2NDQwODI2NzE2MTc4.Xf5Zxg.tUdSXXShHQZZVW7MlWiGu7atUQo")
+
