bf3943d3a80b0ea22f67091b5b2444c4eb013cbf
Ashwin <56057445+Ashwin1172@users.noreply.github.com>
2019-12-22 15:35:37 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,12 @@
 module.exports = client => {
   console.log(`${client.user.username} Ready to playing with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   function randStatus() {
-    let status = [`with ${client.users.size} users`, `on Saucecade`];
+    let status = [`with ${client.users.size} users`, `with Daynite World`];
     let rstatus = Math.floor(Math.random() * status.length);
-    client.user.setActivity(status[rstatus], { type: 'PLAYING' });
+    client.user.setActivity(status[rstatus], { type: 'WATCHING' });
   };
 setInterval(randStatus, 60000);
-  console.log(`${client.user.username} sukses online!`);
+  console.log(`${client.user.username} Bots online!`);
   
   /*hourly cat present :v - youKnowOwO*/ /*ok then - Hazmi35*/
   client.setInterval(() => {
