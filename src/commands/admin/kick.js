c2a0326c67994a4c881e1fca54af2c81f05746bb
Ashwin <56057445+Ashwin1172@users.noreply.github.com>
2020-01-19 17:16:56 +0530

    Update kick.js

--- src/commands/admin/kick.js
+++ src/commands/admin/kick.js
@@ -10,22 +10,22 @@ return message.channel.send(embed);
   if (!message.guild.member(client.user).hasPermission("MANAGE_NICKNAMES")) return message.channel.send(`**${message.author.tag}** Maaf, Rendang Tidak Mempunyai Permissions \`KICK_MEMBERS\` Tolong Beri Permissions \`KICK_MEMBERS\` Terlebih Dahulu.`).then(msg=>msg.delete(5000))
   
   let toKick = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!toKick) return message.channel.sendMessage("Tidak Dapat Menemukan User ! Mohon Mention Terlebih Dahulu !");
+  if(!toKick) return message.channel.sendMessage("> **__SORRY__** Wrong cmd use😉 plz use  `$ban` `mentionuser` `reason`");
   let reason = args.join(" ").slice(22);
-  if (toKick.hasPermission("KICK_MEMBERS")) return message.channel.send("Hmm Dia Tidak Dapat Dikick :(").then(msg => msg.delete(3000));
+  if (toKick.hasPermission("KICK_MEMBERS")) return message.channel.send("I can°t ban this user").then(msg => msg.delete(3000));
   
   if (toKick.highestRole.position < message.guild.member(client.user).highestRole.position) {
    message.guild.member(toKick).kick(reason);
    try {
     if (!reason) {
-      toKick.send(`**${toKick.user.tag}** Kamu Sudah Dikick Dari**${message.guild.name}**`)
+      toKick.send(`**${toKick.user.tag}** You were kick from**${message.guild.name}**`)
     } else {
-      toKick.send(`**${toKick.user.tag}** Kamu Sudah Dikick Dari **${message.guild.name}**
-Alasan: "${reason}"`);
+      toKick.send(`**${toKick.user.tag}** You were kick from **${message.guild.name}**
+Reason: "${reason}"`);
     }
     let embedB = new RichEmbed()
     .setColor('RANDOM')
-    .setTitle('User Sudah Dikick Dari Server')
+    .setTitle('USER KICKED')
     .addField('username', toKick.user.username, true)
     .addField('ID', toKick.id, true)
     message.channel.send(embedB);
@@ -33,7 +33,7 @@ Alasan: "${reason}"`);
     console.log(e.message)
   }
   } else {
-   message.channel.send(`Saya Tidak Bisa Kick **${toKick.user.tag}** Karena Rolenya Lebih Tinggi Dari Saya Atau Rolenya Sama Dengan Saya.`)
+   message.channel.send(`I Can°t Kick **${toKick.user.tag}**`)
   }
 }
  
